428d98d313d5f9586052da80d6fbd5c1a6748137
Oleksandr Menchynskyi <the.mngs@yahoo.com>
2020-01-23 23:05:43 +0200

    Styled button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+const setHover = ({ color }) => {
+  switch (color) {
+    case 'green':
+      return 'linear-gradient(180deg, #58b684 0%, #4b936d 100%)';
+    case 'red':
+      return 'linear-gradient(180deg, #EB5353 0%, #BD4444 100%)';
+    case 'pink':
+      return 'linear-gradient(180deg, #D6BBD9 0%, #C8A3CC 100%)';
+    default:
+      return 'linear-gradient(180deg, #D6BBD9 0%, #C8A3CC 100%)';
+  }
+};
+
 const ButtonStyled = styled.button`
-  background-color: ${({ theme, color }) => theme.colors[color]};
+  height: 40px;
+  padding: 12px 44px;
+  border: none;
+  border-radius: 6px;
+  background: ${({ theme, color }) => theme.colors[color]};
   color: ${({ theme, primary }) => (primary ? theme.colors.textPrimary : theme.colors.dark)};
+  font-size: 14px;
+
+  &:hover {
+    cursor: pointer;
+    background: ${setHover};
+  }
+
+  &:focus {
+    outline: none;
+  }
 `;
 
 export const Button = props => <ButtonStyled {...props}>{props.children}</ButtonStyled>;
