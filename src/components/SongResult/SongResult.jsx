cdddf85025210c16cc73b9d62db3d203807e3344
Oleksandr Menchynskyi <the.mngs@yahoo.com>
2020-01-25 17:49:27 +0200

    Song result styled

--- src/components/SongResult/SongResult.jsx
+++ src/components/SongResult/SongResult.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { SongResultBox, FrameBox, InfoBox, SongInfo, AlbumInfo } from './SongResultStyled';
+import { SongResultBox, FrameBox, InfoBox, SongInfo, AlbumInfo, SongName, SongArtist } from './SongResultStyled';
 
 export const SongResult = () => {
   const currentSong = useSelector(state => state.currentSong);
@@ -15,14 +15,14 @@ export const SongResult = () => {
           frameBorder="0"
           allowTransparency="true"
           src={`https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=300&height=300&color=ff0000&layout=dark&size=medium&type=tracks&id=${deezerId}`}
-          width="300"
-          height="300"
+          width="180"
+          height="180"
         />
       </FrameBox>
       <InfoBox>
         <SongInfo>
-          <span>{songName}</span>
-          <span>{artist}</span>
+          <SongName>{songName}</SongName>
+          <SongArtist>{artist}</SongArtist>
         </SongInfo>
         <AlbumInfo>
           <span>Album:</span>
--- src/components/SongResult/SongResultStyled.js
+++ src/components/SongResult/SongResultStyled.js
@@ -2,26 +2,52 @@ import styled from 'styled-components';
 
 export const SongResultBox = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+  margin-bottom: 2rem;
+  width: 100%;
 `;
 
 export const FrameBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  box-shadow: 30px 34px 134px rgba(0, 0, 0, 0.35);
+  border-radius: 6px;
+  overflow: hidden;
 `;
 
 export const InfoBox = styled.div`
   display: flex;
   flex-flow: column wrap;
   justify-content: space-around;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 export const SongInfo = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 1.5rem;
 `;
 
 export const AlbumInfo = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.textSecondary};
+  font-size: 14px;
+`;
+
+export const SongName = styled.span`
+  color: ${({ theme }) => theme.colors.textPrimary};
+  font-size: 18px;
+  margin-bottom: 5px;
+`;
+
+export const SongArtist = styled.span`
+  color: ${({ theme }) => theme.colors.textSecondary};
+  font-size: 14px;
 `;
