5d2c4da2527a364406d99dc5715b7283c5ef914f
Oleksandr Menchynskyi <the.mngs@yahoo.com>
2020-01-25 16:32:35 +0200

    Song result styled

--- src/components/SongResult/SongResult.jsx
+++ src/components/SongResult/SongResult.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { SongResultBox, FrameBox, InfoBox, SongIfo, AlbumInfo } from './SongResultStyled';
 
 export const SongResult = () => {
-  const songId = useSelector(state => state.currentSong.id);
+  const currentSong = useSelector(state => state.currentSong);
+  const { deezerId, artist, songName, album } = currentSong;
 
   return (
-    <div>
-      <iframe
-        title="deezer"
-        scrolling="no"
-        frameBorder="0"
-        allowTransparency="true"
-        src={`https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=ff0000&layout=dark&size=medium&type=tracks&id=${songId}`}
-        width="700"
-        height="350"
-      />
-      <span>Song Name</span>
-      <span>Song Author</span>
-      <span>Album:</span>
-      <span>Album name</span>
-    </div>
+    <SongResultBox>
+      <FrameBox>
+        <iframe
+          title="deezer"
+          scrolling="no"
+          frameBorder="0"
+          allowTransparency="true"
+          src={`https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=300&height=300&color=ff0000&layout=dark&size=medium&type=tracks&id=${deezerId}`}
+          width="300"
+          height="300"
+        />
+      </FrameBox>
+      <InfoBox>
+        <SongIfo>
+          <span>{songName}</span>
+          <span>{artist}</span>
+        </SongIfo>
+        <AlbumInfo>
+          <span>Album:</span>
+          <span>{album}</span>
+        </AlbumInfo>
+      </InfoBox>
+    </SongResultBox>
   );
 };
