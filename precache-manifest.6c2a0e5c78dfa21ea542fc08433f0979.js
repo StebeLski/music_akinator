7a02ff85d9ba3d32c7510d9e4b4cbac426658658
stebeLski <antonstebelski@gmail.com>
2020-01-26 00:22:32 +0200

    Updates

---
+++ precache-manifest.6c2a0e5c78dfa21ea542fc08433f0979.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb901503aa30e6081d4795cb7b7158b9",
+    "url": "/music_akinator/index.html"
+  },
+  {
+    "revision": "86c5adc310c07d83be0d",
+    "url": "/music_akinator/static/js/2.115b5411.chunk.js"
+  },
+  {
+    "revision": "300a629c8a57ac75c5352cba730be3c8",
+    "url": "/music_akinator/static/js/2.115b5411.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d542f203a36d57ae05c7",
+    "url": "/music_akinator/static/js/main.712b7290.chunk.js"
+  },
+  {
+    "revision": "0ab26ae559653a34a466",
+    "url": "/music_akinator/static/js/runtime-main.86fbd364.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.864b9d942fafa65119453ac2718eb24f.js"
+  "/music_akinator/precache-manifest.6c2a0e5c78dfa21ea542fc08433f0979.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/music_akinator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
